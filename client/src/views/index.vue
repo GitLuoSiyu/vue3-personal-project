8b03ce65f8ae974815e31b84c55819445cdd4bd3
GitLuoSiyu <1514813390@qq.com>
2019-04-28 00:59:35 +0800

    add vue3

---
+++ client/src/views/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="index">
+    初始化页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {};
+  },
+  props: [],
+  created() {},
+  methods: {}
+};
+</script>
+
+<style></style>
