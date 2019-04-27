902cd27750f5562740bd299e8c9857e0257286a3
GitLuoSiyu <1514813390@qq.com>
2019-04-28 01:34:31 +0800

    add 404

--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-import Index from "./views/index.vue";
+import Index from "./views/Index.vue";
+import Register from "./views/Register.vue";
+import NotFound from "./views/404.vue";
 
 Vue.use(Router);
 
@@ -17,6 +19,16 @@ export default new Router({
       path: "/index",
       name: "index",
       component: Index
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register
+    },
+    {
+      path: "*",
+      name: "/404",
+      component: NotFound
     }
   ]
 });
---
+++ client/src/views/Register.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="register">
+    <section class="form_container">
+      <div class="manage_tip">
+        <span class="title">久保田在线后台管理系统</span>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "register",
+  comments: {},
+  data() {
+    return {};
+  },
+  props: [],
+  created() {},
+  methods: {}
+};
+</script>
+
+<style scoped>
+.register {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: url(./../assets/images/bg.jpg) no-repeat center center;
+  background-size: 100% 100%;
+}
+.form_container {
+  width: 370px;
+  height: 210px;
+  position: absolute;
+  top: 10%;
+  left: 34%;
+  padding: 25px;
+  border-radius: 5px;
+  text-align: center;
+}
+.form_container .manage_tip .title {
+  font-family: "Microsoft YaHei";
+  font-weight: bold;
+  font-size: 26px;
+  color: #fff;
+}
+</style>
